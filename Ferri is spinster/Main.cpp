ca75a1ed3bf41258b6bdf55479436b6eeab88239
riciprini <karabba00@gmail.com>
2018-04-22 11:43:57 +0200

    Add some function

--- Ferri is spinster/Main.cpp
+++ Ferri is spinster/Main.cpp
@@ -5,39 +5,82 @@
 #include <stdlib.h>
 #include <string>
 #include "Studenti.h"
-void writeOnFile() {
 
-	ofstream myfile;
-	myfile.open("C:\\Users\\Rici\\Desktop\\MYDATABASE.txt", ios::app);
-	Studenti studente;
-	vector<Studenti> s;
-	s.push_back(studente);
-	vector<Studenti>::iterator it;
-	for (it = s.begin();it != s.end();++it) {
-		myfile << *it << endl;
+Studenti students;
+void color() {
+	int colore=students.color();
+	switch (colore) {
+	case 0:
+		system("color 9");
+		system("cls");
+		break;
+	case 1:
+		system("color A");
+		system("cls");
+		break;
+	case 2:
+		system("color E");
+		system("cls");
+		break;
+	case 3:
+		system("color C");
+		system("cls");
+		break;
+	default:
+		cout << "Error" << endl;
+
 	}
-	myfile.close();
 
 }
-void readFromFile() {
-	string line;
-	ifstream myfile("C:\\Users\\Rici\\Desktop\\MYDATABASE.txt");
-	if (myfile.is_open())
-	{
-		while (getline(myfile, line))
-		{
-			cout << line << '\n';
-		}
-		myfile.close();
+void addStudent() {
+	int voto;
+	string nome;
+	cout << "Inserisci nome alunno" << endl;
+	cin >> nome;
+	cout << "Inserisci voto alunno" << endl;
+	cin >> voto;
+	if (voto >= 10 && voto <= 0) {
+		cout << "Inserisci un voto compreso tra 10 e 0" << endl;
+		system("cls");
+		return;
 	}
+	system("cls");
+	Studenti s(voto,nome);
+	
+	students.writeOnFile(s);
+}
+void menu() {
+	int scelta;
+	while (true) {
+	cout << "1.Visualizza registro" << endl;
+	cout << "2.Aggiungi alunno" << endl;
+	cout << "3.Cambia colore registro" << endl;
+	cout << "4.Cancella registro" << endl;
+		cin >> scelta;
+		system("cls");
+		switch (scelta) {
+		case 1:
+			students.readFromFile();
+			break;
+		case 2:
+			addStudent();
+			break;
+		case 3:
+			color();
+			break;
+		case 4:
+			students.deleteRegister();
+			break;
+		default:
+			cout << "Error" << endl;
 
-	else cout << "Unable to open file";
-
+		}
+	}
 
 }
 void main() {
-	writeOnFile();
-	readFromFile();
+	
+	menu();
 	getchar();
 
 }
--- Ferri is spinster/Studenti.h
+++ Ferri is spinster/Studenti.h
@@ -6,24 +6,111 @@
 #include <stdlib.h>
 #include <string>
 using namespace std;
-class Studenti {
+class Classe {
+public:
+	Classe() :_nome("4b"), _Studenti(22) {};
+	Classe(string nome, int Studenti) : _nome(nome), _Studenti(Studenti) {};
+	int getStudenti() { return _Studenti; }
+	string getNome() { return _nome; }
+	enum colori {
+		blue,
+		green,
+		yellow,
+		red,
+
+	};
+	virtual const int andamento() = 0;
 protected:
-	int voti;
-	string nome;
+	int _Studenti;
+	string _nome;
+
+	
+};
+class Studenti : public Classe{
+protected:
+	int _voti;
+	string _nome;
 
 public:
-	Studenti() {
-		voti = 10;
-		nome = "Rici";
-
-
-	}
-	int getVoto() { return voti; }
-	string getNome() { return nome; }
+	Studenti():Classe() {
+		_voti = 10;
+		_nome = "Rici";
+}
+	Studenti(int voti, string nome) :_voti(voti), _nome(nome) {};
+	int getVoto() { return _voti; }
+	string getNome() { return _nome; }
+	
+	virtual const int andamento() { return 10; }
 	friend ostream& operator<<(ostream& os, Studenti& rhs) {
 
-		os << rhs.getNome() << " " << rhs.getVoto() << endl;
+		os << rhs.getNome() << ":" << rhs.getVoto() << endl;
 		return os;
 	}
+	void writeOnFile(const Studenti studente)const {
 
+		ofstream myfile;
+		myfile.open("C:\\Users\\Rici\\Desktop\\MYDATABASE.txt", ios::app);
+		vector<Studenti> s;
+		s.push_back(studente);
+		vector<Studenti>::iterator it;
+		for (it = s.begin();it != s.end();++it) {
+			myfile << *it << endl;
+		}
+		myfile.close();
+
+	}
+	void readFromFile()const {
+		string line;
+		ifstream myfile("C:\\Users\\Rici\\Desktop\\MYDATABASE.txt");
+		if (myfile.is_open())
+		{
+			while (getline(myfile, line))
+			{
+				cout << line << '\n';
+			}
+			myfile.close();
+		}
+
+		else cout << "Unable to open file";
+
+
+	}
+	void deleteRegister() {
+		ofstream myfile;
+		myfile.open("C:\\Users\\Rici\\Desktop\\MYDATABASE.txt", ios::out);
+		myfile << "";
+		myfile.close();
+	}
+	int color() {
+		
+		int scelta;
+		cout << "" << endl;
+		cout << "1:BLUE" << endl;
+		cout << "2:GREEN" << endl;
+		cout << "3:YELLOW" << endl;
+		cout << "4:RED" << endl;
+		cin >> scelta;
+		switch (scelta) {
+		case 1:
+			return blue;
+			system("cls");
+			break;
+		case 2:
+			return green;
+			system("cls");
+			break;
+		case 3:
+			return yellow;
+			system("cls");
+			break;
+		case 4:
+			return red;
+			system("cls");
+			break;
+		default:
+			cout << "Error";
+
+
+		}
+	}
 };
